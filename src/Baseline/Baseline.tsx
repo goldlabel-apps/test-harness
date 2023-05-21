7a289039357f89700db6927c0610be232f1369e1
milky <listingslab@gmail.com>
2023-05-21 08:55:15 +0200

    Add firebase to host the harness

---
+++ src/Baseline/Baseline.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Baseline() {
+  return (
+    <div>
+      <header>
+        <h2>Test Harness</h2>
+        <a
+          href="https://github.com/listingslab-goldlabel/test-harness"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Git Repo
+        </a>
+        
+      </header>
+    </div>
+  )
+}
+
+export default Baseline
