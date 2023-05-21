fc486045668a3b13363bcedd763a190f308c6de3
milky <listingslab@gmail.com>
2023-05-21 09:34:37 +0200

    Update a little bit of theme

---
+++ src/Baseline/types.d.ts
@@ -0,0 +1,10 @@
+// import { PaletteMode } from "@mui/material"
+
+export type ThemeShape = {
+    title: string
+    description?: string
+    primaryColor: string
+    secondaryColor: string
+    font: string
+    // mode: PaletteMode
+}
